69cc6b06890052cd905ddef30df272b3931e98cb
Tomy Murphy <39192349+TomyMurphy@users.noreply.github.com>
2024-03-18 14:57:27 -0300

    Add files via upload

---
+++ web-comidas/script.js
@@ -0,0 +1,16 @@
+let menuVisible = false;
+//Función que oculta o muestra el menú
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+function seleccionar(){
+    //oculto el menú una vez que selecciono una opción
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
